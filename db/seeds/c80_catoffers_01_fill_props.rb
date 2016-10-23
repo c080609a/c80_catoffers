8c02f508584f72d8d105175e91e9ac72f414d2c4
C80609A <c080609a@gmail.com>
2016-10-23 17:02:41 +0700

    develop: добавил стандартные свойства thumb_md\sm\lg.

--- db/seeds/c80_catoffers_01_fill_props.rb
+++ db/seeds/c80_catoffers_01_fill_props.rb
@@ -2,12 +2,16 @@
 
 C80Catoffers::Prop.delete_all
 C80Catoffers::Prop.create!({
-                               per_row:3,
-                               preview_width:250, # NOTE:: [x090] синхронизировано с css
-                               preview_height:164, # NOTE:: [x090] синхронизировано с css
-                               big_one_width:621, # NOTE:: [x090] синхронизировано с css
-                               big_one_height: 377, # NOTE:: [x090] синхронизировано с css
-                               thumb_sm_width:80,
-                               thumb_sm_height: 50,
+                               per_row:         3,
+                               preview_width:   80,     # NOTE:: размер иконки в iconed списке # NOTE:: [x090] синхронизировано с css
+                               preview_height:  50,    # NOTE:: размер иконки в iconed списке # NOTE:: [x090] синхронизировано с css
+                               big_one_width:   621,     # NOTE:: [x090] синхронизировано с css
+                               big_one_height:  377,    # NOTE:: [x090] синхронизировано с css
+                               thumb_sm_width:  80,     # NOTE:: просто стандартный размер
+                               thumb_sm_height: 50,     # NOTE:: просто стандартный размер
+                               thumb_md_width:  250,     # NOTE:: просто стандартный размер
+                               thumb_md_height: 160,     # NOTE:: просто стандартный размер
+                               thumb_lg_width:  1000,     # NOTE:: просто стандартный размер
+                               thumb_lg_height: 550,    # NOTE:: просто стандартный размер
                                positions_count: 4
                            })
