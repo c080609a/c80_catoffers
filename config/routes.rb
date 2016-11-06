119d453f4dd5585642eb6dfe841532660082b3e0
C80609A <c080609a@gmail.com>
2016-11-06 17:37:34 +0300

    develop: добавил фейковый root_url, чтобы url_helper работал.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
 C80Catoffers::Engine.routes.draw do
 
-  # get 'offers/(:offer_slug)', :to => 'site#view_gallery'
-
-  # match 'news_guru', :to => 'application#guru', :via => :post
-  # match 'rb', :to => 'banners#counter', :via => :post
-
-  # get '/auth/:provider/callback', to: 'sessions#create'
-  # delete '/logout', to: 'sessions#destroy'
-
-  # resources :comments, :only => :create
+  root to: 'application#fake_main' # NOTE:: добавлен только чтобы работал UrlsHelper
+  match 'offers_guru', :to => 'application#offers_guru', :via => :post
 
 end
