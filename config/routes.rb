68c67441c454918df2e9b85fc5aff4110237f0ba
C80609A <c080609a@gmail.com>
2016-11-07 14:09:12 +0300

    develop: отменил коммит с фейковым маршрутом.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 C80Catoffers::Engine.routes.draw do
 
-  root to: 'application#fake_main' # NOTE:: добавлен только чтобы работал UrlsHelper
+  # root to: 'application#fake_main' # NOTE:: добавлен только чтобы работал UrlsHelper
   match 'offers_guru', :to => 'application#offers_guru', :via => :post
 
 end
