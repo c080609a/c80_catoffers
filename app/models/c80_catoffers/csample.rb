de06165267d988316d5118f57244186e067974bd
C80609A <c080609a@gmail.com>
2016-11-07 16:08:33 +0300

    develop: csample: добавил def_order, добавил after_create :recalc_summ_price, добавил all_widgeted и csphoto_img (для определения размеров картинки ДО рендера вью).

--- app/models/c80_catoffers/csample.rb
+++ app/models/c80_catoffers/csample.rb
@@ -9,18 +9,31 @@ module C80Catoffers
     has_and_belongs_to_many :csprops
     mount_uploader :csphoto, CsphotoUploader
 
+    scope :def_order, -> {order(:ord => :asc)}
+
     after_update :recalc_summ_price
+    after_create :recalc_summ_price
 
     def recalc_summ_price
 
       sum = crows.all
-                .map { |crow| crow.offer.price_integer.to_i }
+                .map { |crow| crow.offer_price_integer.to_i }
                 .inject(0) { |s, x| s + x }
 
       Rails.logger.debug "[TRACE] <C80Catoffers.recalc_summ_price> sum = #{sum}"
 
       self.update_column(:summ_price, sum)
 
+    end
+
+    # выдать список тех Примеров, которые должны выводиться в виджете
+    def self.all_widgeted
+      self.joins(:csprops)
+    end
+    
+    def csphoto_img
+      self.csphoto.present? ? MiniMagick::Image.open(self.csphoto.path):nil
+      # [img["width"],img["height"]]
     end
 
   end
