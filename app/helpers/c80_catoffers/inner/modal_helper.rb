17aeb4c47190d3aab4558f6af9425c9eedb24b57
C80609A <c080609a@gmail.com>
2016-11-13 18:28:01 +0300

    feature_ajax_price: PriceHelper: render_popup_price возвращает bootstrap окно с прайсом - кликабельным списком всех услуг.

--- app/helpers/c80_catoffers/inner/modal_helper.rb
+++ app/helpers/c80_catoffers/inner/modal_helper.rb
@@ -2,8 +2,13 @@ module C80Catoffers
   module Inner
     module ModalHelper
 
-      def _render_bs_modal
-        render :partial => 'c80_catoffers/bs_modal'
+      def _render_bs_modal(content='', title='')
+        render :partial => 'c80_catoffers/bs_modal',
+               :locals => {
+                   title: title,
+                   content: content,
+                   close_label: 'Закрыть'
+               }
       end
 
     end
