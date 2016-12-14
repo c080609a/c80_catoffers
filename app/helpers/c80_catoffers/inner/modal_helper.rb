70b1ec9032468737418b1676e3a9904f43235ef9
C80609A <c080609a@gmail.com>
2016-12-14 10:39:34 +0300

    develop: render_bs_modal: Если имеется загруженный прайс (в виде файла) - допишем к title ссылку на скачивание.

--- app/helpers/c80_catoffers/inner/modal_helper.rb
+++ app/helpers/c80_catoffers/inner/modal_helper.rb
@@ -3,6 +3,15 @@ module C80Catoffers
     module ModalHelper
 
       def _render_bs_modal(content='', title='')
+
+        # Если имеется загруженный прайс (в виде файла) - допишем к title ссылку на скачивание
+        if Price.all.count > 0
+          p = Price.first
+          file_url = p.file.url
+          file_ext = File.extname(file_url).split('.').join('')
+          title = "#{title} (<a href='#{file_url}' title='Откроется в новом окне' target='_blank'>скачать #{file_ext}</a>)"
+        end
+
         render :partial => 'c80_catoffers/bs_modal',
                :locals => {
                    title: title,
